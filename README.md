572163e6409fe1f122252dcc742b574c41ff79e4
Paulo Netto <pauloestrella1994@gmail.com>
2020-10-02 10:01:19 -0300

    curso-docker aula 1 a 57

---
+++ README.md
@@ -0,0 +1,3 @@
+# Curso-docker
+
+Exercícios baseados no curso de [docker](https://www.udemy.com/course/curso-docker/)
---
+++ worker/worker.py
@@ -0,0 +1,13 @@
+import redis, json
+from time import sleep
+from random import randint
+
+
+if __name__=='__main__':
+    r = redis.Redis(host='queue', port=6379, db=0)
+    while True:
+        mensagem = json.loads(r.blpop('sender')[1])
+        # Simulando o envio do email
+        print('Mandando mensagem: ', mensagem['assunto'])
+        sleep(randint(15, 45))
+        print('Mensagem', mensagem['assunto'], 'enviada')
