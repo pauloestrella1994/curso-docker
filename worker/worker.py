5113b0f8f70b3a2f4893235ee41034166f5f3a1e
Paulo Netto <pauloestrella1994@gmail.com>
2020-10-02 11:49:20 -0300

    Aula 59

--- worker/worker.py
+++ worker/worker.py
@@ -1,10 +1,11 @@
-import redis, json
+import redis, json, os
 from time import sleep
 from random import randint
 
 
 if __name__=='__main__':
-    r = redis.Redis(host='queue', port=6379, db=0)
+    redis_host = os.getenv('REDIS_HOST', 'queue')
+    r = redis.Redis(host=redis_host, port=6379, db=0)
     print('Aguradando mensagens...')
     while True:
         mensagem = json.loads(r.blpop('sender')[1])
